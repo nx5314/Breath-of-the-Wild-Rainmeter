4eb1a91f8ffabc49c6fdece5df9147d05059a3d5
cube_guy <leyelm@gmail.com>
2017-06-06 23:11:26 -0700

    Update notification is functional, but slow and/or doesn't update without a manual refresh. Further testing required. Update's loading icon animated. Update link functional. Tips toggle variable in place. Reddit toggle in place.

---
+++ BOTW/@Resources/retrieve_version.lua
@@ -0,0 +1,10 @@
+function Initialize()
+  dofile(SKIN:GetVariable('@') .. "JSON.lua")
+end
+
+function Update()
+  local page_raw = SKIN:GetMeasure('MeasureOnlineVersion'):GetStringValue()
+  local page = decode_json(page_raw)
+  if page == nil then return end
+  SKIN:Bang("!SetVariable", "ONLINEVERSION", page.version)
+end
