0618732472f846211683a2622d05abb615e56252
Maurdekye <ncolaprete@gmail.com>
2017-06-08 19:01:25 -0400

    Added faulty weather implementation

--- BOTW/@Resources/redditfetch.lua
+++ BOTW/@Resources/redditfetch.lua
@@ -2,14 +2,30 @@ function Initialize()
   dofile(SKIN:GetVariable('@') .. "JSON.lua")
   dofile(SKIN:GetVariable('@') .. "gametips.lua")
   previous_links = {}
+  weather_index = 0
 end
 
 function Update()
+  -- Set all relevant meters and variables to default
   SKIN:Bang('!SetOption', 'MeterTitle', 'Text', "")
   SKIN:Bang('!SetOption', 'MeterTip', 'Text', "")
   SKIN:Bang('!SetOption', 'MeterBackdrop', 'LeftMouseDownAction', "")
   SKIN:Bang('!SetVariable', 'CurrentTip', '-1')
 
+  -- Fetch IP and Location for weather tips
+  local ip = SKIN:GetMeasure('MeasureIPWebsite'):GetStringValue()
+  SKIN:Bang('!SetVariable', 'WANIP', ip)
+  SKIN:Bang('!WriteKeyValue', 'Variables', 'WANIP', ip)
+  SKIN:Bang('!UpdateMeasure', 'MeasureLocationWebsite')
+
+  local page_raw = SKIN:GetMeasure('MeasureLocationWebsite'):GetStringValue()
+  local page = decode_json(page_raw)
+  if page == nil then return end
+  local location = page.data.geo.city .. ", " .. page.data.geo.region
+  SKIN:Bang('!SetVariable', 'WeatherLocation', location)
+  SKIN:Bang('!WriteKeyValue', 'Variables', 'WeatherLocation', location)
+
+  -- Decide which tip to show
   local tip_choices = {}
 
   if SKIN:GetVariable('RedditTips') ~= '0' then tip_choices[#tip_choices + 1] = SourceRedditForTip end
@@ -31,7 +47,42 @@ function SourceIngameTip()
 end
 
 function SourceWeatherForTip()
-  
+  print("ip: " .. SKIN:GetVariable('WANIP'))
+  print("location: " .. SKIN:GetVariable('WeatherLocation'))
+  local page_raw = SKIN:GetMeasure('MeasureWeatherWebsite'):GetStringValue()
+  local page = decode_json(page_raw)
+  if page == nil then return end
+  if page.query == nil then return end
+  local weather_info = page.query.results.channel
+  if weather_index == 0 then
+    SKIN:Bang('!SetOption', 'MeterTitle', 'Text', "Today's Weather")
+    local tip_text = weather_info.item.condition.temp .. " degrees, " .. weather_info.item.condition.text .. ". "
+    tip_text = tip_text .. "Wind speeds of " .. weather_info.wind.speed .. weather_info.units.speed
+    tip_text = tip_text .. " and " .. weather_info.atmosphere.humidity .. "% humidity."
+    SKIN:Bang('!SetOption', 'MeterTip', 'Text', tip_text)
+  elseif weather_index == 1 then
+    SKIN:Bang('!SetOption', 'MeterTitle', 'Text', "Tomorrow's Forecast")
+    local tomorrow = weather_info.item.forecast[1]
+    local tip_text = tomorrow.text .. "; highs of " .. tomorrow.high .. " degrees and lows of " .. tomorrow.low .. " degrees."
+    SKIN:Bang('!SetOption', 'MeterTip', 'Text', tip_text)
+  elseif weather_index == 2 then
+    SKIN:Bang('!SetOption', 'MeterTitle', 'Text', "Five-day Forecast")
+    local tip_text = ""
+    for i=1,5 do
+      local day_weather = weather_info.item.forecast[i]
+      tip_text = tip_text .. day_weather.day .. ": " .. day_weather.high .. " degrees, " .. day_weather.text .. ".  "
+    end
+    SKIN:Bang('!SetOption', 'MeterTip', 'Text', tip_text)
+  elseif weather_index == 3 then
+    SKIN:Bang('!SetOption', 'MeterTitle', 'Text', "Ten-day Forecast")
+    local tip_text = ""
+    for i=1,10 do
+      local day_weather = weather_info.item.forecast[i]
+      tip_text = tip_text .. day_weather.day .. ": " .. day_weather.text .. ".  "
+    end
+    SKIN:Bang('!SetOption', 'MeterTip', 'Text', tip_text)
+  end
+  weather_index = (weather_index + 1) % 4
 end
 
 function SourceRedditForTip()
