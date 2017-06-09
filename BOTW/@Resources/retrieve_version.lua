9fb5a15f4005978a0b93eae923a46085ab2eb7e9
cube_guy <leyelm@gmail.com>
2017-06-08 17:44:35 -0700

    Removed old unused script.

--- BOTW/@Resources/retrieve_version.lua
+++
@@ -1,9 +0,0 @@
-function Initialize()
-  dofile(SKIN:GetVariable('@') .. "JSON.lua")
-end
-
-function Update()
-  local page_raw = SKIN:GetMeasure('MeasureOnlineVersion'):GetStringValue()
-  local page = decode_json(page_raw)
-  if page == nil then return end
-end
